987b171708f63042bfc5599fa8913e24b9985e8d
kincadeburr43 <Chimpboy43@gmail.com>
2023-11-07 18:39:12 -0500

    reg and grade apis created

--- src/main/java/com/service/studentvueservice/StudentvueServiceApplication.java
+++ src/main/java/com/service/studentvueservice/StudentvueServiceApplication.java
@@ -1,12 +1,15 @@
 package com.service.studentvueservice;
 
 import com.service.studentvueservice.controlers.FamilyNameController;
+import com.service.studentvueservice.controlers.RegistrationController;
+import com.service.studentvueservice.controlers.gradeController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses= FamilyNameController.class)
+@ComponentScan(basePackageClasses={FamilyNameController.class, RegistrationController.class, gradeController.class})
 public class StudentvueServiceApplication {
 
 	public static void main(String[] args) {
